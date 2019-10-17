1540da8b459526e2626558a9177db9d3c5e2424c
Gustavo Arantes <gustavo@arantes.xyz>
2019-10-17 13:10:28 -0300

    Added fontawesome and table to show the existing classes

--- frontend/src/components/Classes/Classes.js
+++ frontend/src/components/Classes/Classes.js
@@ -1,12 +1,80 @@
 import "./Classes.css";
 
 import React, { Component } from "react";
+import { Table } from "reactstrap";
+
+import { SendRequest } from "../../imports/sendrequest";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class Classes extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            classes: []
+        };
+
+        this._setStateClasses();
+    }
+
+    async _setStateClasses() {
+        let response = [];
+        try {
+            response = await SendRequest("/turmas", "GET");
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+        this.setState({ classes: response });
+    }
+
     render() {
         return (
             <div className="classes">
                 <h1 className="text-center">Lista todas as turmas</h1>
+                <div className="center margin-top">
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Modalidade</th>
+                                <th>Horários</th>
+                                <th>Alunos</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.classes.length > 0 &&
+                                this.state.classes.map((item) => (
+                                    <tr key={item._id}>
+                                        <td>{item.name}</td>
+                                        <td>{item.description}</td>
+                                        <td>{item.modality}</td>
+                                        <td>
+                                            <a href="#">Ver horários</a>
+                                        </td>
+                                        <td>
+                                            {(item.studens &&
+                                                item.students.length) ||
+                                                0}
+                                            /{item.maxNumOfStudents}
+                                        </td>
+                                        <td>
+                                            <a href="#">
+                                                <FontAwesomeIcon icon="edit" />{" "}
+                                            </a>
+                                            <a href="#">
+                                                <FontAwesomeIcon icon="trash-alt" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </Table>
+                    {!this.state.classes.length && (
+                        <p className="text-center">Carregando...</p>
+                    )}
+                </div>
             </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrashAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrashAlt, faEdit);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
